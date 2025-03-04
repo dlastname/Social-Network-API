fb04981b8e1ae99f9dede3280c3c5036525095e4
dlastname <dllorens28@gmail.com>
2025-03-03 22:09:16 -0500

    having some hard to squash bugs... Saving for now, will ask about this in office hours tomorrow

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -1,14 +1,16 @@
-import { Schema, model, Document, ObjectId, Types } from "mongoose";
-import reactionSchema from "./Reaction";
+import { Schema, model, Document, Types } from "mongoose";
+import reactionSchema, { IReaction } from "./Reaction"; // Import the Reaction schema and interface
 
+// Define the Thought interface
 interface IThought extends Document {
   thoughtText: string;
   createdAt: Date;
-  // Single username refering to the User model
-  username: string;
-  reactions: Types.Subdocument;
+  // Single username referring to the User model
+  username: string; 
+  // Array of Reaction subdocuments
+  reactions: Types.DocumentArray<IReaction>; 
   // Virtual property to get the number of reactions
-  reactionCount: number;
+  reactionCount: number; 
 }
 
 const thoughtSchema = new Schema<IThought>(
@@ -23,26 +25,26 @@ const thoughtSchema = new Schema<IThought>(
       type: Date,
       default: Date.now,
     },
-    username: [
-      {
-        type: String,
-        required: true,
-        ref: "User",
-      },
-    ],
-    reactions: [reactionSchema],
+    username: {
+      type: String,
+      required: true,
+// Reference to the User model
+      ref: "User", 
+    },
+    // Bug fix: Embed the Reaction schema as an array
+    reactions: [reactionSchema], 
   },
   {
     toJSON: {
-      virtuals: true,
+      virtuals: true, 
     },
-    id: false,
+    id: false, 
   }
 );
 
-// Virtual property `friendCount` to get the number of friends
-thoughtSchema.virtual('reactionCount').get(function () {
-    return this.reactions.length;
+// Virtual property `reactionCount` to get the number of reactions
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
 });
 
 // Initialize the Thought model
